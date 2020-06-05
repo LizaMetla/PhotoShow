d3d43ff389f6a6d6b3787f7c5b8ed56b36a71020
liza.metla <liza.metla@gmail.com>
2020-06-05 17:30:05 +0300

    editing cell in rows

---
+++ PhotoShow/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoShow
+//
+//  Created by Елизавета Метла on 6/5/20.
+//  Copyright © 2020 Елизавета Метла. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
